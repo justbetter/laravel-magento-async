ad644d209bbbfe034b3eebaae84daf345c54aa86
Vincent Boon <vincent@justbetter.nl>
2024-08-27 15:26:11 +0200

    Pint

--- src/Actions/CleanBulkRequests.php
+++ src/Actions/CleanBulkRequests.php
@@ -5,6 +5,7 @@ namespace JustBetter\MagentoAsync\Actions;
 use Illuminate\Database\Eloquent\Builder;
 use JustBetter\MagentoAsync\Contracts\CleansBulkRequests;
 use JustBetter\MagentoAsync\Enums\OperationStatus;
+use JustBetter\MagentoAsync\Models\BulkOperation;
 use JustBetter\MagentoAsync\Models\BulkRequest;
 
 class CleanBulkRequests implements CleansBulkRequests
@@ -14,16 +15,19 @@ class CleanBulkRequests implements CleansBulkRequests
         /** @var int $cleanupHours */
         $cleanupHours = config('magento-async.cleanup');
 
+        // Delete completed statuses
+        BulkOperation::query()
+            ->where('status', '=', OperationStatus::Complete)
+            ->delete();
+
         BulkRequest::query()
-            // Delete where there are no more operations with a non-final status
-            ->where(function (Builder $query) use ($cleanupHours): void {
-                $query->whereDoesntHave('operations', function (Builder $query): void {
-                    $query
-                        ->whereNull('status')
-                        ->orWhere('status', '=', OperationStatus::Open);
-                })->orWhere('created_at', '<', now()->subHour()); // Never delete within an hour
+            // Delete where there are no more operations
+            ->where(function (Builder $query): void {
+                $query
+                    ->whereDoesntHave('operations')
+                    ->where('created_at', '<', now()->subHour()); // Never delete within an hour
             })
-            // Always delete after 7 days
+            // Always delete after configured cleanup time
             ->orWhere('created_at', '<', now()->subHours($cleanupHours))
             ->delete();
     }
